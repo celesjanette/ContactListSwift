8695b7e9d72ff70819e56afc625059488723bb34
celesjanette <celesaug1@gmail.com>
2024-03-25 10:32:44 -0400

    Table created

---
+++ ContactListSwift/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  ContactListSwift
+//
+//  Created by Celes Augustus on 3/24/24.
+//
+
+import Foundation
+struct Constants{
+    static let kSortField = "sortField"
+    static let kSortDirectionAscending = "sortDirectionAscending"
+}
